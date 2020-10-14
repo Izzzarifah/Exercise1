fa735ace57f5e7a09986634f0d56a1b1b7bdc2a9
Izzzarifah <72841552+Izzzarifah@users.noreply.github.com>
2020-10-14 17:24:48 +0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+//Author : Izz Zarifah Zulkefli
 public class Driver {
 
 	public static void main(String [] arg) {
@@ -7,13 +8,27 @@ public class Driver {
 		Fish nemo = new Fish();     //nemo is object, fish is class
 
 		nemo.Swim();
-		nemo.color = "red";  //tukar value
+		//nemo.color = "red";  //tukar value
+		nemo.SetColor("red");    //change here
 		nemo.Swim();
+		nemo.Eat();    //superclass call subclass
 
-		Fish dory = new Fish();   //another object
-		dory.Swim();    //output akan sama ngn default (yg pertama) sbb diaefau
-        dory.color = "yellow";
-        dory.Swim();
+
+
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharpTeeth(130);
+		fiery.Eat();
+
+		//Fish dory = new Fish();   //another object
+		//dory.Swim();    //output akan sama ngn default (yg pertama) sbb diaefau
+        //dory.color = "yellow";
+        //dory.SetColor("yellow");    //change here
+        //dory.Swim();
+
+        //Aquirium fancyAquirium = new Aquirium();
+        //fancyAquirium.fillFish();
 
 	}
 
